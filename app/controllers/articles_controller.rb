10bd29c4d7f7d5340970eaa24643baeb30378fab
erikyoung <erik.young@rmit.edu.vn>
2017-06-04 14:15:18 +0700

    created edit functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,6 +24,20 @@ def create
 	redirect_to root_path
  end
 
+ def show
+ 	@article = Article.find(params[:id])
+ end
+
+ def edit
+ 	@article = Article.find(params[:id])
+ end
+
+ def update
+ 	@article = Article.find(params[:id])
+ 	@article.update_attributes(article_params)
+ 	redirect_to root_path
+ end
+
  private
 
  def article_params
