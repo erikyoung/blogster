4500fdf5562429bec100482c589b3e2d0f153f8a
erikyoung <erik.young@rmit.edu.vn>
2017-06-04 12:38:10 +0700

    added bootstrap functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,21 @@ class ArticlesController < ApplicationController
 
 def index
 	@articles = Article.all
+	@articles = Article.paginate(:page => params[:page], per_page: 2)
 end
+
+def new 
+	 @article = Article.new
+end
+
+def create
+	Article.create(article_params)
+	redirect_to root_path
+ end
+
+ private
+
+ def article_params
+ 	params.require(:place).permit(:title, :body)
 
 end
