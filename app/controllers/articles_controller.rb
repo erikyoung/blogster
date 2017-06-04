494087c9ad241c57ced1979d4e817c35eba6b2e5
erikyoung <erik.young@rmit.edu.vn>
2017-06-04 14:20:58 +0700

    added delete functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -38,6 +38,12 @@ def create
  	redirect_to root_path
  end
 
+ def destroy
+ 	 @article = Article.find(params[:id])
+ 	 @article.destroy
+ 	 redirect_to root_path
+ end
+
  private
 
  def article_params
